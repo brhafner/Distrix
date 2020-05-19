377820de76db1d8151f9d811bd7469a67b388f17
Ben Hafner <brhafner@gmail.com>
2020-05-18 23:13:29 -0700

    CA Distrix Chart rendering on DOM

---
+++ public/javascripts/index.js
@@ -0,0 +1,164 @@
+// const axios = require('axios');
+// const chart = require('./main_map');
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     // chart.write("chartdiv");
+
+// })
+
+var chartData = [{
+    date: new Date(2011, 5, 1, 0, 0, 0, 0),
+    val: 10
+}, {
+    date: new Date(2011, 5, 2, 0, 0, 0, 0),
+    val: 11
+}, {
+    date: new Date(2011, 5, 3, 0, 0, 0, 0),
+    val: 12
+}, {
+    date: new Date(2011, 5, 4, 0, 0, 0, 0),
+    val: 11
+}, {
+    date: new Date(2011, 5, 5, 0, 0, 0, 0),
+    val: 10
+}, {
+    date: new Date(2011, 5, 6, 0, 0, 0, 0),
+    val: 11
+}, {
+    date: new Date(2011, 5, 7, 0, 0, 0, 0),
+    val: 13
+}, {
+    date: new Date(2011, 5, 8, 0, 0, 0, 0),
+    val: 14
+}, {
+    date: new Date(2011, 5, 9, 0, 0, 0, 0),
+    val: 17
+}, {
+    date: new Date(2011, 5, 10, 0, 0, 0, 0),
+    val: 13
+}];
+
+
+window.onload = function () {
+    var chart = am4core.create("chartdiv", am4maps.MapChart);
+
+    // Set map definition
+    chart.geodata = am4geodata_region_usa_congressional_caLow;
+
+    // Set projection
+    chart.projection = new am4maps.projections.Miller();
+
+    // Create map polygon series
+    var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+    // Make map load polygon (like country names) data from GeoJSON
+    polygonSeries.useGeodata = true;
+
+    // Configure series
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = am4core.color("#74B266");
+
+    // Create hover state and set alternative fill color
+    var hs = polygonTemplate.states.create("hover");
+    hs.properties.fill = am4core.color("#367B25");
+
+
+
+    // var chart = new AmCharts.AmStockChart();
+    // chart.pathToImages = "http://www.amcharts.com/lib/3/images/";
+
+    // var dataSet = new AmCharts.DataSet();
+    // dataSet.dataProvider = chartData;
+    // dataSet.fieldMappings = [{
+    //     fromField: "val",
+    //     toField: "value"
+    // }];
+    // dataSet.categoryField = "date";
+    // chart.dataSets = [dataSet];
+
+    // var stockPanel = new AmCharts.StockPanel();
+    // chart.panels = [stockPanel];
+
+    // var legend = new AmCharts.StockLegend();
+    // stockPanel.stockLegend = legend;
+
+    // var panelsSettings = new AmCharts.PanelsSettings();
+    // panelsSettings.startDuration = 1;
+    // chart.panelsSettings = panelsSettings;
+
+    // var graph = new AmCharts.StockGraph();
+    // graph.valueField = "value";
+    // graph.type = "column";
+    // graph.title = "MyGraph";
+    // graph.fillAlphas = 1;
+    // stockPanel.addStockGraph(graph);
+
+    // var categoryAxesSettings = new AmCharts.CategoryAxesSettings();
+    // categoryAxesSettings.dashLength = 5;
+    // chart.categoryAxesSettings = categoryAxesSettings;
+
+    // var valueAxesSettings = new AmCharts.ValueAxesSettings();
+    // valueAxesSettings.dashLength = 5;
+    // chart.valueAxesSettings = valueAxesSettings;
+
+    // var chartScrollbarSettings = new AmCharts.ChartScrollbarSettings();
+    // chartScrollbarSettings.graph = graph;
+    // chartScrollbarSettings.graphType = "line";
+    // chart.chartScrollbarSettings = chartScrollbarSettings;
+
+    // var chartCursorSettings = new AmCharts.ChartCursorSettings();
+    // chartCursorSettings.valueBalloonsEnabled = true;
+    // chart.chartCursorSettings = chartCursorSettings;
+
+    // var periodSelector = new AmCharts.PeriodSelector();
+    // periodSelector.periods = [{
+    //     period: "DD",
+    //     count: 1,
+    //     label: "1 day"
+    // }, {
+    //     period: "DD",
+    //     selected: true,
+    //     count: 5,
+    //     label: "5 days"
+    // }, {
+    //     period: "MM",
+    //     count: 1,
+    //     label: "1 month"
+    // }, {
+    //     period: "YYYY",
+    //     count: 1,
+    //     label: "1 year"
+    // }, {
+    //     period: "YTD",
+    //     label: "YTD"
+    // }, {
+    //     period: "MAX",
+    //     label: "MAX"
+    // }];
+    // chart.periodSelector = periodSelector;
+
+    // chart.write("chartdiv");
+}
+
+
+
+// Ref Below
+    //  isbn is a computer programming textbook
+//     let isbn = '0201558025';
+//     axios.get(`/books/${isbn}`)
+//         .then((response) => {
+//             console.log(response);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+
+//     let query = "grace hopper";
+//     axios.get(`/search?string=${query}`)
+//         .then((response) => {
+//             console.log(response);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
---
+++ public/javascripts/main_map.js
@@ -0,0 +1,46 @@
+// import * as am4core from "@amcharts/amcharts4/core";
+// import * as am4charts from "@amcharts/amcharts4/charts";
+// // import * as am4maps from "@amcharts/amcharts4/maps"
+
+// // Create chart instance
+// let chart = am4core.create("chartdiv", am4charts.PieChart);
+
+// // Create pie series
+// let series = chart.series.push(new am4charts.PieSeries());
+// series.dataFields.value = "litres";
+// series.dataFields.category = "country";
+
+// // Add data
+// chart.data = [{
+//     "country": "Lithuania",
+//     "litres": 501.9
+// }, {
+//     "country": "Czech Republic",
+//     "litres": 301.9
+// }, {
+//     "country": "Ireland",
+//     "litres": 201.1
+// }, {
+//     "country": "Germany",
+//     "litres": 165.8
+// }, {
+//     "country": "Australia",
+//     "litres": 139.9
+// }, {
+//     "country": "Austria",
+//     "litres": 128.3
+// }, {
+//     "country": "UK",
+//     "litres": 99
+// }, {
+//     "country": "Belgium",
+//     "litres": 60
+// }, {
+//     "country": "The Netherlands",
+//     "litres": 50
+// }];
+
+// // And, for a good measure, let's add a legend
+// chart.legend = new am4charts.Legend();
+
+// export default chart;
